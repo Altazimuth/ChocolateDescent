a78d5ae4d4fb3446e9732edf24d8bf59a24669ec
Max Waine <bobishmax@hotmail.com>
2024-02-18 12:35:07 +1300

    [SDL3] Fix win32main.cpp

--- platform/win32/win32main.cpp
+++ platform/win32/win32main.cpp
@@ -17,7 +17,7 @@ as described in copying.txt.
 #endif
 #include "platform/platform.h"
 
-#include <SDL3/SDL.h> 
+#include <SDL3/SDL_main.h> 
 
 int main(int argc, char** argv) //[ISB] oops, must be called with c linkage...
 {
